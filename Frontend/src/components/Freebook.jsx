3e1b687723cbc913dac044031aa594babf39786f
Snyder21 <pkcoding21@gmail.com>
2024-11-21 14:02:07 +0530

    backend link change

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -12,7 +12,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://bookstall-1.onrender.com/book");
 
         console.log(res.data);
         const data = res.data.filter((data) => data.category === "Free");
